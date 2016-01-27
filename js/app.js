4f5f2de06d2940ecb289ef31076d763d75eb26c5
badillosoft <badillo.soft@hotmail.com>
2016-01-27 01:41:12 -0600

    Se agrego la clase Point y Node y se modifico la clase Scene brindando propiedades.

--- js/app.js
+++ js/app.js
@@ -4,5 +4,16 @@ window.onload = function () {
   scene = new Scene('canvas');
 
   scene.fullScreen();
-  scene.clear();
+  scene.clear('#333');
+
+  var p = new Point();
+
+  for (var y of [100, 200, 300]) {
+    for (var x of [100, 200, 300]) {
+      p.x = x;
+      p.y = y;
+      console.log(p);
+      p.draw(scene.ctx);
+    }
+  }
 };
--- js/core.js
+++ js/core.js
@@ -1,29 +1,6 @@
-// var Scene = (function (window) {
-//   var Scene = function (canvas) {
-//     this.canvas = canvas;
-//     this.ctx = canvas.getContext('2d');
-//   };
-//
-//   Scene.prototype.width = function () {
-//     return this.canvas.width;
-//   };
-//
-//   Scene.prototype.height = function () {
-//     return this.canvas.height;
-//   };
-//
-//   Scene.prototype.fullScreen = function () {
-//     this.canvas.width = window.innerWidth;
-//     this.canvas.height = window.innerHeight;
-//   };
-//
-//   Scene.prototype.clear = function (color) {
-//     this.ctx.fillStyle = color || '#777';
-//     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-//   };
-//
-//   return Scene;
-// })(window);
+/****************************************************************/
+/* Util - Clases útiles                                         */
+/****************************************************************/
 
 class Scene {
   constructor (canvasId) {
@@ -31,12 +8,14 @@ class Scene {
     this.ctx = canvas.getContext('2d');
   }
 
-  width () {
-    return this.canvas.width;
-  }
+  get width () { return this.canvas.width; }
+  set width (value) { this.canvas.width = value; }
 
-  height () {
-    return this.canvas.height;
+  get height () { return this.canvas.height; }
+  set height (value) { this.canvas.height = value; }
+
+  get center () {
+    return { width: this.width / 2, height: this.height / 2 }
   }
 
   fullScreen () {
@@ -47,5 +26,48 @@ class Scene {
   clear (color) {
     this.ctx.fillStyle = color || '#777';
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value || null;
+    this.children = [];
+  }
+
+  add (node) {
+    children.push(node);
+  }
+
+  remove (node) {
+    children.remove(node);
+  }
+
+  apply (method) {
+    this.value[method](arguments.slice(1));
+  }
+}
+
+
+/****************************************************************/
+/* Geometría - Clases para objetos geométricos                  */
+/****************************************************************/
+
+class Point {
+  constructor (x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+  }
+
+  get clone () {
+    return new Point(this.x, this.y);
+  }
+
+  draw (ctx, color) {
+    ctx.save();
+    ctx.fillStyle = color || '#F00';
+    ctx.fillRect(this.x, this.y, 1, 1);
+    ctx.restore();
+    scene.ctx.fill();
   }
 }
